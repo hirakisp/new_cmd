9d6b09e5ce8b6b595686a5a82bb9b5e2cdf1a2d5
hirakisp <dummy@github.com>
2019-02-21 13:51:10 +0900

    modified directory

--- src/debug.h
+++
@@ -1,6 +0,0 @@
-/*
- * debug.h
- */
-
-extern void __printf(char* message,...);
-extern void check_debug_option(void);
--- src/input.h
+++
@@ -1,5 +0,0 @@
-struct input_implement {
-	void *layer_attach();
-	void *layer_init();
-	void *layer_analyze();
-}
